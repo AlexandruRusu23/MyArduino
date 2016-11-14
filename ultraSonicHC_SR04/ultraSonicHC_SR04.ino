2cf56a02a543a51be256126fa3862f4c13e74796
AlexandruRusu23 <specialone47@gmail.com>
2016-11-14 23:18:47 +0200

    ultra sonic sensor sketch

---
+++ ultraSonicHC_SR04/ultraSonicHC_SR04.ino
@@ -0,0 +1,48 @@
+#define TRIG_PIN 7
+#define ECHO_PIN 8
+#define MAX_DIST 23200
+
+void setup() {
+  pinMode(TRIG_PIN, OUTPUT);
+  digitalWrite(TRIG_PIN, LOW);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  unsigned long t1;
+  unsigned long t2;
+  unsigned long pulse_width;
+  float cm;
+  float inches;
+
+  // Hold the trigger pin high for at least 10 ms
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // Wait for pulse on echo pin
+  while ( digitalRead(ECHO_PIN) == 0 );
+
+  // Measure how long the echo pin was held high (pulse width)
+  // Note: the micros() counter will overflow after ~70 min
+  t1 = micros();
+  while ( digitalRead(ECHO_PIN) == 1);
+  t2 = micros();
+  
+  pulse_width = t2 - t1;
+  cm = pulse_width / 58.0;
+  inches = pulse_width / 148.0;
+
+  if ( pulse_width > MAX_DIST ) 
+    Serial.println("Out of range"); 
+  else 
+  {
+    Serial.print(cm);
+    Serial.print(" cm \t");
+    Serial.print(inches);
+    Serial.println(" in");
+  }
+  
+  delay(60);
+}
