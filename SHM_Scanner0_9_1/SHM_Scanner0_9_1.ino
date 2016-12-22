d5190969acbae7125385f0cc8601ba6eff3fa860
AlexandruRusu23 <alexandru.rusu707@gmail.com>
2016-12-22 14:22:47 +0200

    command manager sketch

--- SHM_Scanner0_9_1/SHM_Scanner0_9_1.ino
+++ SHM_Scanner0_9_1/SHM_Scanner0_9_1.ino
@@ -12,89 +12,7 @@ void setup()
 
 void loop()
 {
-  getSerial();
-  switch(serialData)
-  {
-    case 1:
-    {
-      Serial.println("Analog/Digital write");
-      //analog digital write
-      getSerial();
-      switch(serialData)
-      {
-        case 1:
-        {
-          Serial.println("Analog write");
-          // analog write
-          getSerial();
-          pinNumber = serialData;
-          getSerial();
-          analogRate = serialData;
-          pinMode(pinNumber, OUTPUT);
-          analogWrite(pinNumber, analogRate);
-          pinNumber = 0;
-          break;
-        }
-        case 2:
-        {
-          Serial.println("Digital write!");
-          //digital write
-          getSerial();
-          pinNumber = serialData;
-          Serial.print("Pin number: ");
-          Serial.println(pinNumber);
-          getSerial();
-          digitalState = serialData;
-          pinMode(pinNumber, OUTPUT);
-          if (digitalState == 0)
-          {
-            Serial.println("Turn OFF!");
-            digitalWrite(pinNumber, 0);
-          }
-          if (digitalState == 1)
-          {
-            Serial.println("Turn ON!");
-            digitalWrite(pinNumber, 255);
-          }
-          pinNumber = 0;
-          break;
-        }
-      }
-      break;
-    }
-    case 2:
-    {
-      getSerial();
-      switch (serialData)
-      {
-      case 1:
-        {
-          //analog read
-          getSerial();
-          pinNumber = serialData;
-          pinMode(pinNumber, INPUT);
-          sensorVal = analogRead(pinNumber);
-          Serial.println(sensorVal);
-          sensorVal = 0;
-          pinNumber = 0;
-          break;
-        } 
-      case 2:
-        {
-          //digital read
-          getSerial();
-          pinNumber = serialData;
-          pinMode(pinNumber, INPUT);
-          sensorVal = digitalRead(pinNumber);
-          Serial.println(sensorVal);
-          sensorVal = 0;
-          pinNumber = 0;
-          break;
-        }
-      }
-      break;
-    }
-  }
+  CommandManager();
 }
 
 long getSerial()
@@ -112,5 +30,115 @@ long getSerial()
 
   inByte = 0;
   return serialData;
+}
+
+void CommandManager()
+{
+  getSerial();
+  switch(serialData)
+  {
+    case 1:
+    {
+      Serial.println("Analog/Digital write");
+      //analog digital write
+      getSerial();
+      switch(serialData)
+      {
+        case 1:
+        {
+          AnalogWrite();
+          break;
+        }
+        case 2:
+        {
+          DigitalWrite();
+          break;
+        }
+      }
+      break;
+    }
+    case 2:
+    {
+      Serial.println("Analog/Digital read");
+      getSerial();
+      switch (serialData)
+      {
+      case 1:
+        {
+          AnalogRead();
+          break;
+        } 
+      case 2:
+        {
+          DigitalRead();
+          break;
+        }
+      }
+      break;
+    }
+  }
+}
+
+void AnalogRead()
+{
+  Serial.println("Analog read!");
+  getSerial();
+  pinNumber = serialData;
+  Serial.print("Pin number: ");
+  Serial.println(pinNumber);
+  pinMode(pinNumber, INPUT);
+  sensorVal = analogRead(pinNumber);
+  Serial.println(sensorVal);
+  sensorVal = 0;
+  pinNumber = 0;
+}
+
+void AnalogWrite()
+{
+  Serial.println("Analog write");
+  getSerial();
+  pinNumber = serialData;
+  getSerial();
+  analogRate = serialData;
+  pinMode(pinNumber, OUTPUT);
+  analogWrite(pinNumber, analogRate);
+  pinNumber = 0;
+}
+
+void DigitalRead()
+{
+  Serial.println("Digital read");
+  getSerial();
+  pinNumber = serialData;
+  Serial.print("Pin number: ");
+  Serial.println(pinNumber);
+  pinMode(pinNumber, INPUT);
+  sensorVal = digitalRead(pinNumber);
+  Serial.println(sensorVal);
+  sensorVal = 0;
+  pinNumber = 0;
+}
+
+void DigitalWrite()
+{
+  Serial.println("Digital write!");        
+  getSerial();
+  pinNumber = serialData;
+  Serial.print("Pin number: ");
+  Serial.println(pinNumber);
+  getSerial();
+  digitalState = serialData;
+  pinMode(pinNumber, OUTPUT);
+  if (digitalState == 0)
+  {
+    Serial.println("Turn OFF!");
+    digitalWrite(pinNumber, 0);
+  }
+  if (digitalState == 1)
+  {
+    Serial.println("Turn ON!");
+    digitalWrite(pinNumber, 255);
+  }
+  pinNumber = 0;
 }
 
